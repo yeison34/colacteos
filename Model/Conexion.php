0c7f10a7982fada3817feb34a91497366c0ad30c
yeison34 <jeisonperenguez1998@gmail.com>
2022-08-28 21:06:43 -0500

    deploy

--- Model/Conexion.php
+++ Model/Conexion.php
@@ -1,9 +1,9 @@
 <?php
     class Conexion{
-        private $server="localhost";
-        private $database="colacteosdb";
-        private $password="3424";
-        private $user="postgres";
+        private $server="ec2-18-215-96-22.compute-1.amazonaws.com";
+        private $database="dbfhk2s6f5jgfn";
+        private $password="4011d4d9cd395072d8f3d3bf02d922b363761e820f7419597b46147a976b04ef";
+        private $user="nbpncaoutwtzkt";
         private $conexion;
 
         public function __construct(){
